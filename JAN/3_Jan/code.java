cf26e5b68a0e2cc817be4a0582b00d3fd403c8d3
YASH MALI <yashmalighub45@gmail.com>
2025-01-03 06:30:56 +0530

    First solution added

---
+++ JAN/3_Jan/code.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int waysToSplitArray(int[] nums) {
+        int n = nums.length;
+        long[] prefix = new long[n];
+        prefix[0] = nums[0];
+        for(int i= 1 ; i < n ; i++){
+            prefix[i] = prefix[i-1] + nums[i];
+        }
+        int count=0;
+        for(int i = 0 ; i < n - 1 ;i++){
+            long left_sum = prefix[i] ;
+            long right_sum = prefix[n-1] - left_sum;  
+            if(left_sum >= right_sum){
+                count++;
+            }
+        }
+        return count;
+    }
+}
